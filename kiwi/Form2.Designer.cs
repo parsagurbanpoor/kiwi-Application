3a63d48351aa20094cedf2e4e07da9e8caba79c8
parsagurbanpoor <parsagurbanpoor@gmail.com>
2026-08-03 14:19:59 +0330

    add book managment

--- kiwi/Form2.Designer.cs
+++ kiwi/Form2.Designer.cs
@@ -31,12 +31,14 @@
             this.panelsidebar = new System.Windows.Forms.Panel();
             this.panelmain = new System.Windows.Forms.Panel();
             this.btndashbored = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.panelsidebar.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelsidebar
             // 
             this.panelsidebar.BackColor = System.Drawing.Color.SeaGreen;
+            this.panelsidebar.Controls.Add(this.button1);
             this.panelsidebar.Controls.Add(this.btndashbored);
             this.panelsidebar.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelsidebar.Location = new System.Drawing.Point(0, 0);
@@ -58,15 +60,29 @@
             this.btndashbored.Dock = System.Windows.Forms.DockStyle.Top;
             this.btndashbored.FlatAppearance.BorderSize = 0;
             this.btndashbored.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btndashbored.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btndashbored.ForeColor = System.Drawing.Color.White;
             this.btndashbored.Location = new System.Drawing.Point(0, 0);
             this.btndashbored.Name = "btndashbored";
             this.btndashbored.Size = new System.Drawing.Size(200, 50);
             this.btndashbored.TabIndex = 0;
-            this.btndashbored.Text = "DashBored";
-            this.btndashbored.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btndashbored.Text = "Dashbored";
             this.btndashbored.UseVisualStyleBackColor = true;
             this.btndashbored.Click += new System.EventHandler(this.btndashbored_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Book Management";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // DashBoredpage
             // 
@@ -77,7 +93,7 @@
             this.Controls.Add(this.panelsidebar);
             this.Name = "DashBoredpage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "DashBored";
+            this.Text = "Dashbored";
             this.Load += new System.EventHandler(this.DashBoredpage_Load);
             this.panelsidebar.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -89,5 +105,6 @@
         private System.Windows.Forms.Panel panelsidebar;
         private System.Windows.Forms.Button btndashbored;
         private System.Windows.Forms.Panel panelmain;
+        private System.Windows.Forms.Button button1;
     }
 }
